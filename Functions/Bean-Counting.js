7f9ed6c60bf96ad3171b5158932ce0698dcef92a
KariukiBrian <brayoknight@gmail.com>
2021-01-19 17:21:59 -0800

    bean-counting

---
+++ Functions/Bean-Counting.js
@@ -0,0 +1,13 @@
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  function countBns(string) {
+    return countChar(string, "B");
+  }
